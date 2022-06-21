2aba03e7c4fe1d1a18f0dc6ae64c2ca5839d6a0d
167710 <101186945+167710@users.noreply.github.com>
2022-06-21 10:49:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main
+{
+    
+  public static void main(String[] args)
+    {     
+    int i;
+	System.out.println ("Pierwsze 500 liczb naturalnych:\n");
+	for (i=1;i<=500;i++)
+	{      
+		System.out.println (i);
+	}
+System.out.println ("\n");
+}
+}
